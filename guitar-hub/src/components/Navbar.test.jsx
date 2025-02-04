0c7541edeadebe0ae09597c12da7e2085ccc0392
darenagos <103580328+darenagos@users.noreply.github.com>
2025-02-04 15:05:41 +0000

    Implement testing with Jest, through babel
    Test for navbar component

---
+++ guitar-hub/src/components/Navbar.test.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import Navbar from './Navbar';
+import { MemoryRouter } from 'react-router-dom';
+
+test('renders the Navbar component', () => {
+    render(
+    <MemoryRouter>
+        <Navbar />
+    </MemoryRouter>
+ );
+    const homepageLink = screen.getByText('Homepage');
+    const learningLink = screen.getByText('Learning');
+    const chordDiagramsLink = screen.getByText('Chord Diagrams');
+    const mySongsLink = screen.getByText('My Songs');
+
+    expect(homepageLink).toBeInTheDocument();
+    expect(learningLink).toBeInTheDocument();
+    expect(chordDiagramsLink).toBeInTheDocument();
+    expect(mySongsLink).toBeInTheDocument();
+});
