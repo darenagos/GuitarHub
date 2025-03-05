9c3639a97258cb28abbfe321f5542e33a8a8e9a2
darenagos <103580328+darenagos@users.noreply.github.com>
2025-03-05 17:05:34 +0000

    refactoring file/folder structure

--- guitar-hub/src/App.jsx
+++ guitar-hub/src/App.jsx
@@ -1,24 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import ErrorBoundary from "./ErrorBoundary";
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homepage from "./routes/Homepage.jsx";
-import LearningPage from "./routes/LearningPage.jsx";
-import ChordDiagramsPage from "./routes/ChordDiagramsPage.jsx";
-import MySongsPage from "./routes/MySongsPage.jsx";
+import AppRoutes from "./routes/index.jsx";
+import { BrowserRouter } from "react-router";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/homepage" element={<Homepage />} />
-        <Route path="/learning" element={<LearningPage />} />
-        <Route path="/chord-diagrams" element={<ChordDiagramsPage />} />
-        <Route path="/my-songs" element={<MySongsPage />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
