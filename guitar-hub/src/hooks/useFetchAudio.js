1da4f1baaf181c3fdb4fb5b8e6c0c58138d081d3
darenagos <103580328+darenagos@users.noreply.github.com>
2025-04-04 13:38:28 +0100

    custom hook useFetchAudio added and implemented

---
+++ guitar-hub/src/hooks/useFetchAudio.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+const API_KEY = "05955013";
+
+const useFetchAudio = (trackId) => {
+  const [audioUrl, setAudioUrl] = useState(null);
+
+  useEffect(() => {
+    const fetchAudio = async () => {
+      if (!trackId) return;
+      try {
+        const response = await fetch(
+          `https://api.jamendo.com/v3.0/tracks/?client_id=${API_KEY}&format=jsonpretty&limit=1&id=${trackId}`
+        );
+        const data = await response.json();
+
+        if (data.results && data.results.length > 0) {
+          const track = data.results[0];
+          setAudioUrl(track.audio);
+        } else {
+          console.error("Track not found for the provided track ID.");
+        }
+      } catch (error) {
+        console.error("Error fetching audio:", error);
+      }
+    };
+
+    fetchAudio();
+  }, [trackId]);
+
+  return audioUrl;
+};
+
+export default useFetchAudio;
--- guitar-hub/src/pages/SongDetailPage.jsx
+++ guitar-hub/src/pages/SongDetailPage.jsx
@@ -6,52 +6,16 @@ import useFetchSong from "../hooks/useFetchSong";
 import SongDetails from "../components/LearningComponents/LearningSongDetailPageComponents/SongDetails";
 import AudioPlayer from "../components/LearningComponents/LearningSongDetailPageComponents/AudioPlayer";
 import ChordDisplaySection from "../components/LearningComponents/LearningSongDetailPageComponents/ChordDisplaySection";
+import useFetchAudio from "../hooks/useFetchAudio";
 
 const API_KEY = "05955013";
 
 const SongDetailPage = () => {
   const { id } = useParams();
   const { song, loading: songLoading, error: songError } = useFetchSong(id);
-
-  const [audioUrl, setAudioUrl] = useState(null);
   const [currentSecond, setCurrentSecond] = useState(0);
-
   const navigate = useNavigate();
-
-  // Fetch track audio from Jamendo API
-  useEffect(() => {
-    if (song?.artist) {
-      console.log("song jamendo id:", song.jamendo_id);
-      fetchAudio(song.jamendo_id);
-    }
-  }, [song]);
-
-  useEffect(() => {
-    if (song && song.chord_sequence) {
-      console.log("Chord sequence for the song:", song.chord_sequence);
-    }
-  }, [song]);
-  const fetchAudio = async (trackId) => {
-    try {
-      const response = await fetch(
-        `https://api.jamendo.com/v3.0/tracks/?client_id=${API_KEY}&format=jsonpretty&limit=1&id=${trackId}`
-      );
-      console.log("response", response);
-      const data = await response.json();
-
-      if (data.results && data.results.length > 0) {
-        const track = data.results[0];
-        const audioUrl = track.audio;
-
-        console.log("Audio URL:", audioUrl);
-        setAudioUrl(audioUrl);
-      } else {
-        console.error("Track not found for the provided track ID.");
-      }
-    } catch (error) {
-      console.error("Error fetching audio:", error);
-    }
-  };
+  const audioUrl = useFetchAudio(song?.jamendo_id);
 
   const handleTimeUpdate = (e) => {
     setCurrentSecond(Math.floor(e.target.currentTime)); // Update current second
